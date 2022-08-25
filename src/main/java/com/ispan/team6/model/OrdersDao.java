5b5252850ad8d9b40f195cab0acf4e725f855c53
java284284 <110451853+java284284@users.noreply.github.com>
2022-08-25 13:58:41 +0800

    使用者訂單歷史 OrderDetail

--- src/main/java/com/ispan/team6/model/OrdersDao.java
+++ src/main/java/com/ispan/team6/model/OrdersDao.java
@@ -1,11 +1,17 @@
 package com.ispan.team6.model;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.ispan.team6.entity.Orders;
 
 public interface OrdersDao extends JpaRepository<Orders, Integer> {
+	
+	@Query(value = "select * from Orders where fk_users_id =:id", nativeQuery = true)
+	public List<Orders> findOrdersByUsersId(int id);
+
 
 //	@Query(value = "select * from orders whrere ")
 	
