5fb2cdd06bdc77d73a4098d1c6c26857a1faefbf
Andy <jack998313@gmail.com>
2022-08-25 14:16:57 +0800

    comment service controller調整

--- src/main/java/com/ispan/team6/controller/CommentApi.java
+++ src/main/java/com/ispan/team6/controller/CommentApi.java
@@ -1,8 +1,14 @@
 package com.ispan.team6.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ispan.team6.dto.DishDTO;
+import com.ispan.team6.entity.Comment;
 import com.ispan.team6.service.CommentService;
 
 @RestController
@@ -11,5 +17,10 @@ public class CommentApi {
 	@Autowired
 	private CommentService cService;
 	
-	
+	@GetMapping("/comment")
+	public List<Comment> listAllComment(@RequestParam(value="restId", required=false) Integer restId, @RequestParam(value="showMode", required=false) String mode) {
+		List<Comment> cmt = cService.findCommentByRest(restId);
+		
+		return cmt;
+	}
 }
