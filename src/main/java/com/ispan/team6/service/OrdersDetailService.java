4cdb94f69247621613de51ae9eba016ef5fd0197
java284284 <110451853+java284284@users.noreply.github.com>
2022-08-30 00:38:39 +0800

    (請登入否則寫入失敗)Orders寫入成功，OrdersDetail寫入失敗

--- src/main/java/com/ispan/team6/service/OrdersDetailService.java
+++ src/main/java/com/ispan/team6/service/OrdersDetailService.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.ispan.team6.entity.Dish;
 import com.ispan.team6.entity.OrdersDetail;
+import com.ispan.team6.model.DishDAO;
 import com.ispan.team6.model.OrdersDetailDao;
 
 @Transactional
@@ -17,6 +19,8 @@ public class OrdersDetailService {
 	
 	@Autowired
 	private OrdersDetailDao ordersDetailDao;
+	@Autowired
+	private DishDAO dishDAO;
 
 	
 	public void orderFood(OrdersDetail ordersDetail ) {
@@ -31,5 +35,17 @@ public class OrdersDetailService {
 		}
 
 		return null;
+	}
+	
+//	public Dish findById2(int id);
+//	
+	public Dish OrderFindDishById(int id) {
+		Optional<Dish> Dish = dishDAO.findById(id);
+		if (Dish.isPresent()) {
+			return Dish.get();
+		}
+		 
+		return null;
+		
 	}
 }
